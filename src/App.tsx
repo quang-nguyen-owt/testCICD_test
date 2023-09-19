bd93a09adeafb8549be77f9b9bb96e52e9c0c4d5
Quang Nguyen <qnguyen@owt.swiss>
2023-09-19 15:41:10 +0700

    build file

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Something new
+        Something new OWT
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with QQQ
+          Hello World with QQQ
         </a>
       </header>
     </div>
